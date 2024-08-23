749788126a21d36769157929f9cdea555dd4cbc9
Jeremy <gatwech3211@gmail.com>
2024-08-23 21:40:18 +0300

    Default pro level plan selection ...

--- containWithMostWater.py
+++ containWithMostWater.py
@@ -5,7 +5,6 @@ def maxArea(self, height: List[int]) -> int:
     right = len(height) - 1
     while left < right:
         shorter_line = min(height[left], height[right])
-        
         max_area = max(max_area, shorter_line * (right - left))
         if height[left] < height[right]:
             left += 1
